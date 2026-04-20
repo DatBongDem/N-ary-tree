56fff24552a3adc3840afd60ade2b6985336ec11
DatBongDem <se181844nguyentiendat@gmail.com>
2026-04-20 22:20:58 +0700

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/N-ary-tree/",   
   plugins: [react()],
-  base: "/binary-tree-app/",
-  build: {
-    outDir: "dist"
-  }
 })
